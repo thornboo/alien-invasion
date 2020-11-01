1e1e4781e8ccd77ecd674eb9bbaf69f7fa042848
zhaozhuang <zhaozhuang1210@gmail.com>
2020-11-01 16:20:53 +0800

    2020.11.1

--- Armed_spaceship/alien_invasion.py
+++ Armed_spaceship/alien_invasion.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+sys.path.append("D:\\github\\alien_invasion\\alien")
+from alien import Alien
 
 import pygame
 from pygame.sprite import Group
-
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -22,13 +23,16 @@ def run_game():
     #创建一个用于存储子弹的编组
     bullets = Group()
     
+    #创建一个外星人
+    alien = Alien(ai_settings , screen)
+    
     #开始游戏主循环
     while True:
         #监视键盘和鼠标事件
         gf.check_events(ai_settings , screen , ship , bullets )
         ship.update()
-        gf.update_bullets( bullets )
-        gf.update_screen ( ai_settings , screen , ship , bullets )
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings , screen , ship , alien , bullets )
 
 run_game()
         
