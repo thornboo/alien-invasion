1982e526e727ed1d7b161d806bc5bbc46df94b07
zhaozhuang <zhaozhuang1210@gmail.com>
2020-11-12 23:57:27 +0800

    update

--- Create_alien/alien.py
+++ Create_alien/alien.py
@@ -14,7 +14,8 @@ class Alien_a(Sprite):
         self.dir_name = os.path.dirname(os.path.abspath(__file__))
 
         #加载外星人图像，并设置其rect属性
-        path = pygame.image.load(os.path.join(self.dir_name, "image", "alien.bmp"))
+        path = pygame.image.load(os.path.join(self.dir_name, "image", "alien.b\
+                                              mp"))
         self.image = pygame.transform.scale(path, (20, 20))
         self.rect = self.image.get_rect()
     
