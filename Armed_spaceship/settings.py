9d04ceb64f4ec9f6311d5e97fed033b7525087d5
zhaozhuang <zhaozhuang1210@gmail.com>
2020-11-13 00:14:10 +0800

    update

--- Armed_spaceship/settings.py
+++ Armed_spaceship/settings.py
@@ -19,3 +19,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60 , 60 , 60 #子弹颜色
         self.bullets_allowed = 5  #子弹个数
+        
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #fleet_direction为1表示向右移动，为-1表示向左移动
+        self.fleet_direction = 1
