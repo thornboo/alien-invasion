0f78a7858e16568351607d705e899fc5ccee03da
aons-zhao <zzhao1213@gmail.com>
2020-11-13 00:14:10 +0800

    update

--- Armed_spaceship/game_functions.py
+++ Armed_spaceship/game_functions.py
@@ -69,6 +69,10 @@ def update_bullets(bullets) :
         if bullet.rect.bottom <= 0 :
             bullets.remove ( bullet )
             
+    #检查是否有子弹击中外星人
+    #如果有，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+            
 def get_number_aliens_x(ai_settings, alien_width):
     """计算每行可以容纳多少个外星人"""
     available_space_x = ai_settings.screen_width - 2 * alien_width
@@ -101,6 +105,28 @@ def create_fleet(ai_settings , screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
+            
+def update_aliens(aliens):
+    """"更新外星人群中所有外星人的位置"""
+    aliens.update()
+    
+def check_fleet_edges(ai_settings, aliens):
+    """有外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+        
+def change_fleet_direction(ai_settings, aliens):
+    """将整群外星人下移，并改变他们方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+    
+def update_aliens(ai_settings, aliens):
+    """检查是否有外星人位于屏幕边缘，并更新整群外星人的位置"""
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
         
         
         
