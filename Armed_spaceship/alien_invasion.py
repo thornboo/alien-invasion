a582c4bcc43ada6267f31d17523ad0f8ff1062a9
aons-zhao <zzhao1213@gmail.com>
2020-11-13 01:01:30 +0800

    update

--- Armed_spaceship/alien_invasion.py
+++ Armed_spaceship/alien_invasion.py
@@ -36,7 +36,7 @@ def run_game():
         #监视键盘和鼠标事件
         gf.check_events(ai_settings , screen , ship , bullets )
         ship.update()
-        gf.update_bullets(aliens, bullets)
+#        gf.update_bullets(aliens, bullets)
         gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings , screen , ship , aliens , bullets )
 
--- Armed_spaceship/game_functions.py
+++ Armed_spaceship/game_functions.py
@@ -8,7 +8,6 @@ import Create_alien.alien as Aliens
 from bullet import Bullet
 
 
-
 def check_keydown_events(event , ai_settings , screen , ship , bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
@@ -35,17 +34,17 @@ def check_keyup_events(event,ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings , screen , ship, bullet):
+def check_events(ai_settings, screen, ship, bullet):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.time.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             check_keydown_events(event , ai_settings , screen , ship, bullet)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event , ship)
             
-def update_screen(ai_settings , screen , ship , aliens, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     #每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -59,7 +58,7 @@ def update_screen(ai_settings , screen , ship , aliens, bullets):
     #让最近绘制的屏幕可见
     pygame.display.flip()
     
-def update_bullets(bullets) :
+def update_bullets(bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     #更新子弹的位置
     bullets.update()
@@ -87,7 +86,7 @@ def get_number_rows(ai_settings,ship_height, alien_height):
     
 def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     """创建一个外星人并将其放在当前行"""
-    alien = Aliens(ai_settings, screen)
+    alien = Aliens.Alien_a(ai_settings, screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x =alien.x
@@ -98,6 +97,7 @@ def create_fleet(ai_settings , screen, ship, aliens):
     """创建外星人群"""
     #创建一个外星人，并计算一行可以容纳多少个外星人
     #外星人间距为外星人宽度
+    alien = Aliens.Alien_a(ai_settings , screen)
     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
     number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
 
