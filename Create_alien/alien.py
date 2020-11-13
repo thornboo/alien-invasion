e8b38384d44c13f8484078c5179349d02284b901
zhaozhuang <zhaozhuang1210@gmail.com>
2020-11-13 14:52:57 +0800

    modify

--- Create_alien/alien.py
+++ Create_alien/alien.py
@@ -39,6 +39,8 @@ class Alien_a(Sprite):
         screen_rect = self.screen.get_rect()
         if self.rect.right >= screen_rect.right:
             return True
+        elif self.rect.left <= 0:
+            return True
         
             
             
