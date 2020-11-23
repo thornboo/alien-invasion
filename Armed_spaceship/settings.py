43e2a96bbe10d99d8fe55d5bbb595dfa04dd8155
zhaozhuang <zhaozhuang1210@gmail.com>
2020-11-23 23:09:05 +0800

    Set spaceship restrictions and game end conditions

--- Armed_spaceship/settings.py
+++ Armed_spaceship/settings.py
@@ -11,14 +11,15 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # 飞船设置
-        self.ship_speed_factor = 0.5  # 飞船速度
+        self.ship_speed_factor = 1.5  # 飞船速度
+        self.ship_limit = 3  # 飞船个数限制
 
         # 子弹设置
-        self.bullet_speed_factor = 0.4  # 子弹速度
-        self.bullet_width = 5
+        self.bullet_speed_factor = 1  # 子弹速度
+        self.bullet_width = 3
         self.bullet_height = 8
         self.bullet_color = 60, 60, 60  # 子弹颜色
-        self.bullets_allowed = 5  # 子弹个数
+        self.bullets_allowed = 10  # 子弹个数
 
         # 外星人设置
         self.alien_speed_factor = 0.1  # 外星人移动速度
