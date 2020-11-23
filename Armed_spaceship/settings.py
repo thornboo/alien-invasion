09132b7e3c124dcc53186bfcd126daa3eaf1c9b4
zhaozhuang <zhaozhuang1210@gmail.com>
2020-11-23 15:04:58 +0800

    Fixed some bugs

--- Armed_spaceship/settings.py
+++ Armed_spaceship/settings.py
@@ -2,27 +2,27 @@
 
 class Settings():
     """一个储存游戏《外星人入侵》的所有设置的类"""
-    
+
     def __init__(self):
         """初始化游戏的设置"""
         # 屏幕设置￼
         self.screen_width = 900
         self.screen_height = 600
-        self.bg_color = (230 , 230 , 230)
-        
-        #飞船设置
-        self.ship_speed_factor = 0.5  #飞船速度
-        
-        #子弹设置
-        self.bullet_speed_factor = 0.4  #子弹速度
+        self.bg_color = (230, 230, 230)
+
+        # 飞船设置
+        self.ship_speed_factor = 0.5  # 飞船速度
+
+        # 子弹设置
+        self.bullet_speed_factor = 0.4  # 子弹速度
         self.bullet_width = 5
         self.bullet_height = 8
-        self.bullet_color = 60, 60 , 60  #子弹颜色
-        self.bullets_allowed = 5    #子弹个数
-        
-        #外星人设置
-        self.alien_speed_factor = 0.1 #外星人移动速度
+        self.bullet_color = 60, 60, 60  # 子弹颜色
+        self.bullets_allowed = 5  # 子弹个数
+
+        # 外星人设置
+        self.alien_speed_factor = 0.1  # 外星人移动速度
         self.fleet_drop_speed = 10
-        
-        #fleet_direction为1表示向右移动，为-1表示向左移动
+
+        # fleet_direction为1表示向右移动，为-1表示向左移动
         self.fleet_direction = 1
--- Armed_spaceship/ship.py
+++ Armed_spaceship/ship.py
@@ -5,40 +5,40 @@ import pygame
 
 
 class Ship():
-    
-    def __init__(self ,ai_settings ,screen):
+
+    def __init__(self, ai_settings, screen):
         self.screen = screen
         self.ai_settings = ai_settings
-        self.dir_name = os.path.dirname(os.path.abspath(__file__))        
-        
-        #飞船图像并获取其外接矩形
+        self.dir_name = os.path.dirname(os.path.abspath(__file__))
+
+        # 飞船图像并获取其外接矩形
         path = pygame.image.load(os.path.join(self.dir_name, "images", "ship.bmp"))
-        self.image = pygame.transform.scale(path, (20,20))  #飞船图片的尺寸
+        self.image = pygame.transform.scale(path, (20, 20))  # 飞船图片的尺寸
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        
-        #将每艘新飞船放在屏幕底部中央
+
+        # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-        
-        #在飞船的属性center中储存小数值
+
+        # 在飞船的属性center中储存小数值
         self.center = float(self.rect.centerx)
-        
-        #移动标志
+
+        # 移动标志
         self.moving_right = False
         self.moving_left = False
-        
+
     def update(self):
         """根据移动标志调整飞船位置"""
-        #更新飞船的center值，而不是rect
+        # 更新飞船的center值，而不是rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-            
-        #根据self.center更新rect对象
+
+        # 根据self.center更新rect对象
         self.rect.centerx = self.center
-        
+
     def blitme(self):
         """在指定位置绘制飞船"""
-        self.screen.blit(self.image , self.rect)
+        self.screen.blit(self.image, self.rect)
